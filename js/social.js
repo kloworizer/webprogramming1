fa3d405059b16926b1b73655c9f4d95156c1c1d5
kloworizer <esha.indra@gmail.com>
2022-06-10 20:07:25 +0700

    fix url encode

--- js/social.js
+++ js/social.js
@@ -1,5 +1,5 @@
 function setUrl() {
-    url = encodeURI(window.location.href);
+    url = encodeURIComponent(window.location.href);
     document.getElementById("facebook").href = "https://www.facebook.com/sharer/sharer.php?u=" + url;
     document.getElementById("twitter").href = "https://www.twitter.com/share?url=" + url;
     document.getElementById("whatsapp").href = "whatsapp://send?text=" + url;
